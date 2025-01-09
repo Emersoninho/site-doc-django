1f0d4d7cb52272700b3076a319e3da0fe80ef03f
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-09 08:19:39 -0300

    form
    criação do furmulário

--- webapp/settings.py
+++ webapp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
-    'accounts.app.AccountsConfig',
+    'accounts.apps.AccountsConfig',
 
 ]
 
