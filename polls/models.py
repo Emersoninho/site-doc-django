92a3a1747a7a9ae85263a7549f3a6b8ff9023f93
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-09 07:23:23 -0300

    models e migração
    criando a odels e migração

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField('pergunta', max_length=200)
+    pub_date = models.DateTimeField('data de publicação')
