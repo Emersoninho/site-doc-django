cbf13923570d26488dd61ea5614d010ac956b3fc
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-09 00:35:54 -0300

    views
    atualização da views e layout

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 def index(request):
     # Retorne uma resposta HTTP
     #return HttpResponse('Olá Django - index')
-    return render(request, 'index.html')
+    #return render(request, 'index.html')
+    return render(request, 'index.html', {'titulo': 'bem bindo ao sistema de Enquetes'})
 
 def ola(request):
     return HttpResponse('Olá Django')
