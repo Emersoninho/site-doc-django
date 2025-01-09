51abc59fc96e68e5400ccfc1216acf0ca8773701
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-09 08:34:38 -0300

    views do acconts
    views do app acconts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.hashers import make_password
+from django.contrib import messages
+from accounts.forms import AccountsSignupForm
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model
+
+class AccountCreateView(CreateView):
+    model = User
+    template_name = 'registration/signup_form.html'
+    form_class = AccountsSignupForm
+    success_url = reverse_lazy('login')
+    success_message = 'Usuário criado com sucesso!'
+
+    def form_valid(self, form):
+        form.instance.password = make_password(form.instance.password)
+        form.save()
+        messages.success(self.request, self.success_message)
+
+        return super(AccountCreateView, self).form_valid(form)
+    
