4caf577abfecdba84a69a5da8e2ac4c5ad8bea6a
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-09 12:02:00 -0300

    settings
    configuração das mensagens

--- webapp/settings.py
+++ webapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # configuração do login e logout
 LOGIN_REDIRECT_URL = '/ola/'
 LOGOUT_REDIRECT_URL = '/index/'
+
+#menssagens do django
+MESSAGE_TAGS = {
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+    messages.DEBUG: 'secundary',
+}
