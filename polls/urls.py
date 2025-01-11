c6392cae672f8773edbbd7f5b09a01332d1b086a
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-11 16:06:15 -0300

    urls.polls
    urls do app

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from polls.views import index, ola
+from polls.views import index, ola, question_create
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('ola/', ola, name='ola'),
+    path('pergunta_create/', question_create, name='poll_create'),
 ]
