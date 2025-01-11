e595948ab6fa6e9de7924a1b3bf337710c2fe1c0
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-11 20:53:53 -0300

    forms
    forms atualização

--- polls/forms.py
+++ polls/forms.py
@@ -5,3 +5,9 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('question_text', 'pub_date')
+        widgets = {
+            'pub_date': forms.DateInput(
+                format= '%Y-%m-%d',
+                attrs={'type': 'date'}
+            ),
+        }
