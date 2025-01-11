1ed6fea775c61af352eb5719d03ed262e98fca76
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-11 14:46:51 -0300

    models
    renomeando os modelos

--- polls/models.py
+++ polls/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField('pergunta', max_length=200)
     pub_date = models.DateTimeField('data de publicação')
+
+    class Meta:
+        verbose_name = 'pergunta'
+        verbose_name_plural = 'perguntas'
