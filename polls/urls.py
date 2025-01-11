7098958a3f20488441e44e53263108b7596a5a50
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-11 20:58:34 -0300

    urls
    urls do app atualizado

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from polls.views import index, ola, question_create
+from polls.views import index, ola, question_create, question_update
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('ola/', ola, name='ola'),
     path('pergunta_create/', question_create, name='poll_create'),
+    path('pergunta/<int:question_id>/', question_update, name='question_update'),
 ]
