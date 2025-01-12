a4878f5aab25676be7659d78c865c03bf2272a78
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-12 15:35:21 -0300

    urls
    urls de accounts atual

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from accounts import views
 
 urlpatterns = [
     path('accounts/signup/', views.AccountCreateView.as_view(), name='signup'),
+    path('accounts/<>int:pk/edit/', views.AccountUpdateView.as_view(), name='account_edit'),
 ]
