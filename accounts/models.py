4abf9d1dc8bc542aa0273395a9982dba6f220025
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-12 14:44:30 -0300

    models
    models para imagen

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,5 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     data_nascimento = models.DateField('Data de nascimento', null=True, blank=True)
     cpf = models.CharField('CPF', max_length=11, null=True, blank=True)
+    imagem = models.FileField(upload_to='images/user', default=None, null=True, blank=True)
+    
