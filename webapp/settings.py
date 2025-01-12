e12a0f6c90ab28e2d98a1ff4f18907c5529ea855
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-12 14:52:09 -0300

    migrate
    migrate e settings faltando a barra

--- webapp/settings.py
+++ webapp/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
