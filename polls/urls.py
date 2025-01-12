2cf5efc4d1bfab6bf6e3c58de23e51380fb4f633
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-11 22:24:17 -0300

    urls e views
    atualizando a urls e views

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from polls.views import index, ola, question_create, question_update
+from polls.views import index, ola, question_create, question_update, question_delete
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('ola/', ola, name='ola'),
     path('pergunta_create/', question_create, name='poll_create'),
     path('pergunta/<int:question_id>/', question_update, name='question_update'),
+    path('pergunta/<int:question_id>/remove/', question_delete, name='question_remove')
 ]
