a9324495f5407b4a97b9c219385fe3bdad96504e
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-12 15:20:10 -0300

    views
    views accounts atual

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.hashers import make_password
 from django.contrib import messages
 from accounts.forms import AccountsSignupForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -22,3 +23,22 @@ class AccountCreateView(CreateView):
 
         return super(AccountCreateView, self).form_valid(form)
     
+class AccountUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'accounts/user_form.html'
+    fields = ('firstname', 'email', 'imagem',)
+    success_url = reverse_lazy('index')
+    success_message = 'Perfil atualizado com sucesso'
+
+    # editar o própio perfil
+    def get_queryset(self):
+        user_id = self.kwargs.get('pk')
+        user = self.request.user
+        if user is None or not user.is_authenticated or user_id != user_id:
+            return User.objects.none()
+        
+        return user.objects.filter(id=user_id)
+    
+    def form_valid(self, form):
+        messages.success(self.request, self.success_message)
+        return super(AccountCreateView, self).form_valid(form)
