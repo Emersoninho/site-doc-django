1af3145ff4c2c3a4ffe44441bde5a71499ee0dee
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-15 13:48:37 -0300

    models
    models atual

--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,14 @@ class Question(models.Model):
         verbose_name = 'pergunta'
         verbose_name_plural = 'perguntas'
 
+    def __str__(self):
+        return self.question_text
+    
+    def get_total_votes(self):
+        votes = Choice.objects.filter(question=self).aggregate(total=models.sum('votes'))
+
+        return votes.get('total')
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
